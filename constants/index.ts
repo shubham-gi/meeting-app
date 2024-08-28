529b71337fa031393e29e6302485bfa2abab86b9
shubham-gravitii <shubham@gravitii.xyz>
2024-08-29 02:30:32 +0530

    project setup completed

---
+++ constants/index.ts
@@ -0,0 +1,27 @@
+export const sidebarLinks=[
+    {
+        label:'Home',
+        route:'/',
+        imgUrl:'/icons/something.svg',
+    },
+    {
+        label:'Upcoming',
+        route:'/Upcoming',
+        imgUrl:'/icons/Upcoming.svg',
+    },
+    {
+        label:'Previous',
+        route:'/Previous',
+        imgUrl:'/icons/previous.svg',
+    },
+    {
+        label:'Recordings',
+        route:'/Recordings',
+        imgUrl:'/icons/video.svg',
+    },
+    {
+        label:'Personal Room',
+        route:'/personal-room',
+        imgUrl:'/icons/add-personal.svg',
+    },
+]
